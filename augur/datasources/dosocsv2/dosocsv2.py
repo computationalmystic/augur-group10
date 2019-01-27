ebd633f885ddf76763cbe3ef5eeecf6c09ba626f
nebrethar <msnell@unomaha.edu>
2019-01-27 07:33:01 -0800

    Testing new DoSOCSv2 plugin

--- augur/datasources/dosocsv2/dosocsv2.py
+++ augur/datasources/dosocsv2/dosocsv2.py
@@ -3,6 +3,8 @@
 Data source that uses the DoSOCSv2 scanner to identify SPDX license information. 
 """
 
+import re
+from collections import deque
 import subprocess
 import os
 from augur import logger
@@ -19,11 +21,21 @@ class DoSOCSv2(object):
         self.scan('chaoss', 'augur')
 
     def scan(self, owner, repo):
-        repo_url = f'https://github.com/{owner}/{repo}.git'
+        repo_url = 'https://github.com/' + owner + '/' + repo + '.git'
 
         repo_path = os.path.join(self.__repo_folder, repo)
 
-        with open('nomos.txt', 'w') as outfile:
-            subprocess.call(['git', 'clone', repo_url, repo_path], shell=False)
-            subprocess.call(['dosocs2', 'scan', repo_path], shell=False, stdout=outfile)
+        redirect = open(r"temp.txt", "w")
+        subprocess.call(['git', 'clone', repo_url, repo_path], shell=False)
+        LSTRING = subprocess.Popen(['dosocs2', 'oneshot', repo_path], shell=False, stdout=subprocess.PIPE)
+        istr = subprocess.check_output(['dosocs2', 'oneshot', repo_path])
+        direct = open("/home/nebrethar/augur/temp.txt", "r")
+        direct.tell()
+        direct.seek(0)
+        direct.tell()
+        lstr = direct.read()
 
+
+        print("OUTPUTS")
+        print(redirect)
+        print(istr.decode('UTF-8'))
